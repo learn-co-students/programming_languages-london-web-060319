1d41e664032b171c1a53124e75f473cd9ac0a5f2
Federica Macchi <federica.macchi3@gmail.com>
2019-05-28 12:17:54 +0100

    Done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
+
+
 def reformat_languages(languages)
-  # your code here
-end
+  new_hash = {}
+  #iterate over first level old hash
+  languages.each do |style, languages_data|
+    languages_data.each do |language, type_hash|
+      type_hash.each do |type_key, type_value|
+        if style == :functional && language == :javascript
+          new_hash[language][:style][1] = style
+        else
+          new_hash[language] = {type_key => type_value, :style => [style]}
+        end
+      end
+    end
+  end
+  return new_hash
+ end
+
+
